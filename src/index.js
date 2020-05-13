f74306f30b1e05bc36b5178f1155a96cd7b1fd40
Daniel Lugo <danlugo92@gmail.com>
2020-05-13 11:24:26 -0400

    rexport submodules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import * as Schema from './schema'
 import * as Logger from './logger'
+import * as Constant from './constant'
+import * as Utils from './utils'
 
-export { Logger, Schema }
+export { Logger, Schema, Constant, Utils }
