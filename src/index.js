1e27ce482f63f860c4da37759f2d5f5d14ab60ba
Daniel Lugo <danlugo92@gmail.com>
2020-05-13 20:18:45 -0400

    constant -> constants

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as Schema from './schema'
 import * as Logger from './logger'
-import * as Constant from './constant'
+import * as Constants from './constants'
 import * as Utils from './utils'
 
-export { Logger, Schema, Constant, Utils }
+export { Logger, Schema, Constants, Utils }
