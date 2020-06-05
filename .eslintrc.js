fbba6077f4eee48e5115f2ff3fdf591db25ecbac
Daniel Lugo <danlugo92@gmail.com>
2020-06-05 12:33:34 -0400

    change rules, remove unnecesary ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,15 +14,23 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier/@typescript-eslint',
   ],
   rules: {
     // complains about javascript file functions even though it shouldn't
-    '@typescript-eslint/explicit-function-return-type': 0,
-    // hostile rule
-    '@typescript-eslint/no-explicit-any': 0,
+    // if/when the whole codebase is in typescript we'll turn it back on
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+
+    // this is a job for code reviews
+    '@typescript-eslint/ban-ts-comment': 0,
+
+    // these rules are great but lots of reworking needs to be done to comply
+    // with them, so we'll put them aside (as warnings) for now
+    '@typescript-eslint/no-unsafe-assignment': 1,
+    '@typescript-eslint/no-unsafe-return': 1,
+    '@typescript-eslint/no-unsafe-member-access': 1,
+    '@typescript-eslint/no-explicit-any': 1,
   },
 }
