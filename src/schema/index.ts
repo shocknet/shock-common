a53aedd2d06fc654ce10d63be6f64c7741af90fd
Daniel Lugo <danlugo92@gmail.com>
2020-08-05 18:00:22 -0400

    unify schema files

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Contains types that are used throughout the application. Written in
+ * typescript for easier implementation.
+ *
+ * Nominal types are archieved through the enum method as outlined here:
+ * https://basarat.gitbook.io/typescript/main-1/nominaltyping
+ */
 import isFinite from 'lodash/isFinite'
 import isNumber from 'lodash/isNumber'
 import isNaN from 'lodash/isNaN'
@@ -5,22 +12,134 @@ import isNaN from 'lodash/isNaN'
 import * as logger from '../logger'
 import * as Gun from './gun'
 
-import {
-  EncSpontPayment,
-  User,
-  ContentItem,
-  EmbeddedImage,
-  EmbeddedVideo,
-  Paragraph,
-  PostStatus,
-  Post,
-  WallPage,
-  Follow,
-  Wall,
-} from './schema-types'
-
 export { Gun }
-export * from './schema-types'
+
+/**
+ * Exported only for declaration generation. Do not use.
+ * https://github.com/microsoft/TypeScript/issues/9865
+ */
+export enum _EncSpontPayment {
+  _ = '',
+}
+
+/**
+ * Spontaneous payment as found inside a chat message body.
+ */
+export type EncSpontPayment = _EncSpontPayment & string
+
+export interface HasPublicKey {
+  publicKey: string
+}
+
+export interface UserBase {
+  avatar: string | null
+  bio: string | null
+  displayName: string | null
+  lastSeenApp: number
+  lastSeenNode: number
+}
+
+export type User = HasPublicKey & UserBase
+
+export type PartialUser = HasPublicKey & Partial<User>
+
+export interface EmbeddedVideo {
+  type: 'video/embedded'
+
+  magnetURI: string
+
+  width: string
+
+  height: string
+}
+
+export interface EmbeddedImage {
+  type: 'image/embedded'
+
+  magnetURI: string
+
+  width: string
+
+  height: string
+}
+
+export interface Paragraph {
+  type: 'text/paragraph'
+
+  text: string
+}
+
+export type ContentItem = EmbeddedImage | EmbeddedVideo | Paragraph
+
+export type PostStatus = 'draft' | 'publish' | 'private' | 'pending'
+
+export interface PostBase {
+  id: string
+
+  /**
+   * Unix timestamp.
+   */
+  date: number
+
+  /**
+   * Pending means it's being uploaded.
+   */
+  status: PostStatus
+
+  title: string
+
+  /**
+   * Hyphen-delimited tags.
+   */
+  tags: string
+
+  contentItems: Record<string, ContentItem>
+}
+
+/**
+ * Post as seen by its author.
+ */
+export interface Post extends PostBase {
+  author: User
+}
+
+export interface PostN extends PostBase {
+  author: string
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+export interface WallPageBase {
+  count: number
+}
+
+export interface WallPage extends WallPageBase {
+  posts: Record<string, Post>
+}
+
+export interface WallPageN extends WallPageBase {
+  posts: Record<string, string>
+}
+
+export interface WallBase {
+  numOfPages: number
+}
+
+export interface Wall extends WallBase {
+  pages: Record<number, WallPage>
+}
+
+export interface WallN extends WallBase {
+  pages: Record<number, string>
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+export interface Follow {
+  user: string
+  status: 'ok' | 'processing'
+  private: boolean
+}
 
 /**
  * @typedef {import('./schema-types').PartialUser} PartialUser
