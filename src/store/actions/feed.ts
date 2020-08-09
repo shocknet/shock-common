7e9cc6d7427d5b73c0fbb85776a7e7fa21a5d338
Daniel Lugo <danlugo92@gmail.com>
2020-08-09 14:13:31 -0400

    store ids instead of full posts, dispatch page and timestamp

--- src/store/actions/feed.ts
+++ src/store/actions/feed.ts
@@ -10,11 +10,13 @@ export const getMoreBackfeed = () =>
     type: 'getMoreBackfeed',
   } as const)
 
-export const receivedFeed = (posts: Post[]) =>
+export const receivedFeed = (posts: Post[], page: number, timestamp: number) =>
   ({
     type: 'receivedFeed',
     data: {
       posts,
+      page,
+      timestamp,
     },
   } as const)
 
@@ -23,11 +25,17 @@ export const receivedFeed = (posts: Post[]) =>
  * of this list to the head of the current viewport.
  * @param posts
  */
-export const receivedBackfeed = (posts: Post[]) =>
+export const receivedBackfeed = (
+  posts: Post[],
+  page: number,
+  timestamp: number,
+) =>
   ({
     type: 'receivedBackfeed',
     data: {
       posts,
+      page,
+      timestamp,
     },
   } as const)
 
--- src/store/reducers/feed.ts
+++ src/store/reducers/feed.ts
@@ -4,7 +4,7 @@ import * as Schema from '../../schema'
 import * as Actions from '../actions'
 
 export interface State {
-  currentFeed: Schema.Post[]
+  currentFeed: string[]
   latestViewedPostID: string
   lastFetchedPage: number
   lastFetchTimestamp: number
@@ -18,18 +18,28 @@ const reducer: Redux.Reducer<State, Actions.ShockAction> = (
     lastFetchTimestamp: 0,
   },
   action: Actions.ShockAction,
-) => {
+): State => {
   switch (action.type) {
     case 'receivedFeed':
       return {
         ...state,
-        currentFeed: [...state.currentFeed, ...action.data.posts],
+        currentFeed: [
+          ...state.currentFeed,
+          ...Schema.normalizePosts(action.data.posts).result,
+        ],
+        lastFetchedPage: action.data.page,
+        lastFetchTimestamp: action.data.timestamp,
       }
 
     case 'receivedBackfeed':
       return {
         ...state,
-        currentFeed: [...action.data.posts, ...state.currentFeed],
+        currentFeed: [
+          ...Schema.normalizePosts(action.data.posts).result,
+          ...state.currentFeed,
+        ],
+        lastFetchedPage: action.data.page,
+        lastFetchTimestamp: action.data.timestamp,
       }
 
     case '@@redux/INIT' as Actions.ShockAction['type']:
