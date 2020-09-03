3da7f6255ba2e55582d9f78fbaa3ea92087d43c6
Daniel Lugo <danlugo92@gmail.com>
2020-09-03 14:44:18 -0400

    promise and timeout utils

--- src/utils/v2.ts
+++ src/utils/v2.ts
@@ -1,8 +1,26 @@
+import * as Constants from '../constants'
 type BetterExecutor<T> = (
   resolve: (value?: T | PromiseLike<T>) => void,
   reject: (error: Error) => void,
 ) => void
 
-export const makePromise = <T>(executor: BetterExecutor<T>): PromiseLike<T> => {
+export const makePromise = <T>(executor: BetterExecutor<T>): Promise<T> => {
   return new Promise<T>(executor)
+}
+
+export const timeout = <T>(ms: number, promise: Promise<T>): Promise<T> => {
+  let timeoutID: NodeJS.Timeout
+
+  return Promise.race<Promise<T>>([
+    new Promise((_, rej) => {
+      timeoutID = setTimeout(() => {
+        rej(new Error(Constants.ErrorCode.TIMEOUT_ERR))
+      }, ms)
+    }),
+
+    promise.then((v) => {
+      clearTimeout(timeoutID)
+      return v
+    }),
+  ])
 }
