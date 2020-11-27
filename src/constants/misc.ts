07da44c157e3f621bb41c19a40c4263fca96a784
Daniel Lugo <danlugo92@gmail.com>
2020-11-27 15:43:20 -0400

    webclient prefix constant type

--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -8,3 +8,8 @@ export const INITIAL_MSG = '$$__SHOCKWALLET__INITIAL__MESSAGE'
 export const IS_GUN_AUTH = 'IS_GUN_AUTH'
 
 export const NUM_OF_POSTS_PER_WALL_PAGE = 10
+
+export type WebClientPrefix =
+  | 'https://shock.pub'
+  | 'https://lightning.page'
+  | 'https://satoshi.watch'
